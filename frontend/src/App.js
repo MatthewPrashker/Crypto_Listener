8a2019c2b79fdd6bdfc07ecb5d568f2b1edf6fa5
MatthewPrashker <prashker.m@northeastern.edu>
2021-08-26 13:37:01 -0400

    cleaned up src

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
+import emailForm from './components/emailForm';
 import './App.css';
+import ReactDOM from 'react';
 
 function App() {
+  
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <div>
+        <h1>
+          <emailForm />
+        </h1>
+      </div>
+    );
+
+    
+
 }
+
 
 export default App;
--- parser.py
+++ parser.py
@@ -1,6 +1,17 @@
 import urllib
+import xmltodict
 import feedparser
 
-def create_query_string(query: str, start: int, max_results: int) -> str:
-    return ''
+base_url = 'http://export.arxiv.org/api/query?'
+
+
+def create_query_string(author:str, title_keywords:list[str], max_results:int) -> str:
+    base_query = 'search_query'
+    tail_query = ''
+
+
+
+    return base_url + base_query + tail_query
+
+
 
